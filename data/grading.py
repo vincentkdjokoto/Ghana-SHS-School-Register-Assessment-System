81e2dd2a2d1474c5b78c66138127046b27049e8b
VINCENT KOFI DJOKOTO <vincentdjokoto@gmail.com>
2025-12-31 12:49:28 +0000

    Create grading.py

---
+++ data/grading.py
@@ -0,0 +1,19 @@
+def grade(score):
+    if score >= 80:
+        return "A1"
+    elif score >= 70:
+        return "B2"
+    elif score >= 60:
+        return "B3"
+    elif score >= 55:
+        return "C4"
+    elif score >= 50:
+        return "C5"
+    elif score >= 45:
+        return "C6"
+    elif score >= 40:
+        return "D7"
+    elif score >= 35:
+        return "E8"
+    else:
+        return "F9"
